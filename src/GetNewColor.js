561f59c5b17a4e7c46bfe0e6d697fd17740334b5
IgorGryp <igor.gryp@gmail.com>
2023-05-05 15:58:30 +0200

    deleted unecessary code

--- src/GetNewColor.js
+++ src/GetNewColor.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const GetNewColor = () => {
   let symbols = "0123456789ABCDEF";
   let color = "#";
@@ -10,14 +8,6 @@ const GetNewColor = () => {
 
   document.body.style.background = color;
   document.getElementById("hex").innerHTML = color;
-
-  return (
-    <div className="content">
-      <span id="hex">#9980FA</span>
-
-      <button>Get Random Color</button>
-    </div>
-  );
 };
 
 export default GetNewColor;
